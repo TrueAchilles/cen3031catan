3496738bd23c6e53b6fc7db33e7b6428346c3bf3
altendo <altendo@4cb17dc6-836a-11dd-aace-ddc5e87cd236>
2008-10-26 18:22:43 +0000

    Reversion to working copy.

--- Settlers/game/gui/Gui.java
+++ Settlers/game/gui/Gui.java
@@ -9,118 +9,86 @@ import settlers.game.events.*;
 
 public class Gui implements EventListener
 {
-	
-	SettlersGUI gui;
-	
-	public void eventCalled(Event e)
-	{
-		String event = e.getEvent();
-		if (event.equals("PLAYER_INITTURN_START"))
-		{
-			PlayerEvent pe = (PlayerEvent) e;
-			System.out.println("Player init turn: " + pe.player.getID());
-			System.out.println("	Player " + pe.player.getID() + " attempting to place settlement");
+    
+    SettlersGUI gui;
+    
+    public void eventCalled(Event e)
+    {
+        String event = e.getEvent();
+        if (event.equals("PLAYER_INITTURN_START"))
+        {
+            PlayerEvent pe = (PlayerEvent) e;
+            System.out.println("Player init turn: " + pe.player.getID());
+            System.out.println("    Player " + pe.player.getID() + " attempting to place settlement");
             GameState.setActionState(GlobalVar.ACTION_ADD_SETTLEMENT);
-			//sc.buttonSettlement();
-//			PlayerEvent n = new PlayerEvent("PLAYER_INIT_ATTEMPT_SETTLEMENT", pe.player);
-//			EventManager.callEvent(n);
-		}
-		else if (event.equals("PLAYER_INIT_SETTLEMENT_FAIL"))
-		{
-			PlayerEvent pe = (PlayerEvent) e;
-			System.out.println("	Player " + pe.player.getID() + " please place the settlement correctly");
+            //sc.buttonSettlement();
+            gui.getMainBoard().getStatusBar().setText(GameState.getCurPlayer().getName() + ": Please place a settlement");
+//            PlayerEvent n = new PlayerEvent("PLAYER_INIT_ATTEMPT_SETTLEMENT", pe.player);
+//            EventManager.callEvent(n);
+        }
+        else if (event.equals("PLAYER_INIT_SETTLEMENT_FAIL"))
+        {
+            PlayerEvent pe = (PlayerEvent) e;
+            System.out.println("    Player " + pe.player.getID() + " please place the settlement correctly");
 
-			PlayerEvent n = new PlayerEvent("PLAYER_INIT_ATTEMPT_SETTLEMENT", pe.player);
-			EventManager.callEvent(n);
-		}
-		else if (event.equals("PLAYER_INIT_SETTLEMENT_SUCCESS"))
-		{
-			PlayerEvent pe = (PlayerEvent) e;
-			System.out.println("	Player " + pe.player.getID() + " placed settlement");
-			System.out.println("	Player " + pe.player.getID() + " attempting to place road");
+            PlayerEvent n = new PlayerEvent("PLAYER_INIT_ATTEMPT_SETTLEMENT", pe.player);
+            EventManager.callEvent(n);
+        }
+        else if (event.equals("PLAYER_INIT_SETTLEMENT_SUCCESS"))
+        {
+            PlayerEvent pe = (PlayerEvent) e;
+            System.out.println("    Player " + pe.player.getID() + " placed settlement");
+            System.out.println("    Player " + pe.player.getID() + " attempting to place road");
             GameState.setActionState(GlobalVar.ACTION_ADD_ROAD);
-			//sc.buttonRoad();
-			//PlayerEvent n = new PlayerEvent("PLAYER_INIT_ATTEMPT_ROAD", pe.player);
-			//EventManager.callEvent(n);
-		}
-		else if (event.equals("PLAYER_INIT_ROAD_FAIL"))
-		{
-			PlayerEvent pe = (PlayerEvent) e;
-			System.out.println("	Player " + pe.player.getID() + " please place the road correctly");
+            //sc.buttonRoad();
+            gui.getMainBoard().getStatusBar().setText(GameState.getCurPlayer().getName() + ": Please place a road");
+            //PlayerEvent n = new PlayerEvent("PLAYER_INIT_ATTEMPT_ROAD", pe.player);
+            //EventManager.callEvent(n);
+        }
+        else if (event.equals("PLAYER_INIT_ROAD_FAIL"))
+        {
+            PlayerEvent pe = (PlayerEvent) e;
+            System.out.println("    Player " + pe.player.getID() + " please place the road correctly");
 
-			PlayerEvent n = new PlayerEvent("PLAYER_INIT_ATTEMPT_SETTLEMENT", pe.player);
-			EventManager.callEvent(n);
-		}
-		else if (event.equals("PLAYER_INIT_ROAD_SUCCESS"))
-		{
-			PlayerEvent pe = (PlayerEvent) e;
-			System.out.println("	Player " + pe.player.getID() + " placed road");
-			System.out.println("	Player " + pe.player.getID() + " initial turn ends");
-			PlayerEvent n = new PlayerEvent("PLAYER_INITTURN_END", pe.player);
-			EventManager.callEvent(n);
-		}
-		else if (event.equals("PLAYER_TURN_START"))
-		{
-			PlayerEvent pe = (PlayerEvent) e;
-			System.out.println("Player turn: " + pe.player.getID());
-			
-			//Assuming that the player rolls once turn begins
-			PlayerEvent n = new PlayerEvent("PLAYER_ROLL", pe.player);
-			EventManager.callEvent(n);  //Should have a dice animation with the two di values given
-			
-		}
-		else if (event.equals("PLAYER_TRADE_PHASE_BEGIN"))
-		{
+            PlayerEvent n = new PlayerEvent("PLAYER_INIT_ATTEMPT_SETTLEMENT", pe.player);
+            EventManager.callEvent(n);
+        }
+        else if (event.equals("PLAYER_INIT_ROAD_SUCCESS"))
+        {
             PlayerEvent pe = (PlayerEvent) e;
-            
-			//Trade phase commences, player can build, use development cards, or end turn
-			//Therefore need else statements here for these interactions
-			//For now the trade Phase immediately ends
-			PlayerEvent E = new PlayerEvent("PLAYER_TRADE_PHASE_END", pe.player);
-			EventManager.callEvent(E);
-			
-		}
-		else if(event.equals("PLAYER_BUILD_PHASE_BEGIN")) 
-		{
+            System.out.println("    Player " + pe.player.getID() + " placed road");
+            System.out.println("    Player " + pe.player.getID() + " initial turn ends");
+            PlayerEvent n = new PlayerEvent("PLAYER_INITTURN_END", pe.player);
+            EventManager.callEvent(n);
+        }
+        else if (event.equals("PLAYER_TURN_START"))
+        {
             PlayerEvent pe = (PlayerEvent) e;
-			//Player requests to build an object on to the board
-			PlayerEvent E = new PlayerEvent("PLAYER_BUILD_REQUEST", pe.player);
-			EventManager.callEvent(E);
-			
-		}
-		else if(event.equals("PLAYER_REQUEST_BUILD_SUCCESS")) 
-		{
-            PlayerEvent pe = (PlayerEvent) e;
-			//Player was successful and his turn automatically ends
-			PlayerEvent E = new PlayerEvent("PLAYER_TURN_END", pe.player);
-			EventManager.callEvent(E);
-			
-		}
-		else if(event.equals("GAME_END"))
-		{
-			System.out.println("Game is ending...");
-			JOptionPane.showMessageDialog(gui, GameState.players.get(0).getName() + " wins!");
-			System.exit(0);			
-		}
-	}
-	
-	public Gui()
-	{
-		EventManager.registerEvent("PLAYER_INITTURN_START", this);
-		EventManager.registerEvent("PLAYER_TURN_START", this);
-		EventManager.registerEvent("PLAYER_INIT_SETTLEMENT_SUCCESS", this);
-		EventManager.registerEvent("PLAYER_INIT_SETTLEMENT_FAIL", this);
-		EventManager.registerEvent("PLAYER_INIT_ROAD_SUCCESS", this);
-		EventManager.registerEvent("PLAYER_INIT_ROAD_FAIL", this);
-		EventManager.registerEvent("GAME_END", this);
-		EventManager.registerEvent("PLAYER_ROLL", this);
-		EventManager.registerEvent("PLAYER_TRADE_PHASE_BEGIN", this);
-		EventManager.registerEvent("PLAYER_BUILD_PHASE_BEGIN", this);
-		EventManager.registerEvent("PLAYER_REQUEST_BUILD_SUCCESS", this);
-		
+            System.out.println("Player turn: " + pe.player.getID());
+            // in theory, something should happen here
 
-		gui = new SettlersGUI();
-		gui.initialize();		
-	}
-	
+        }
+        else if(event.equals("GAME_END"))
+        {
+            System.out.println("Game is ending...");
+            JOptionPane.showMessageDialog(gui, GameState.players.get(0).getName() + " wins!");
+            System.exit(0);            
+        }
+    }
+
+    public Gui()
+    {
+        EventManager.registerEvent("PLAYER_INITTURN_START", this);
+        EventManager.registerEvent("PLAYER_TURN_START", this);
+        EventManager.registerEvent("PLAYER_INIT_SETTLEMENT_SUCCESS", this);
+        EventManager.registerEvent("PLAYER_INIT_SETTLEMENT_FAIL", this);
+        EventManager.registerEvent("PLAYER_INIT_ROAD_SUCCESS", this);
+        EventManager.registerEvent("PLAYER_INIT_ROAD_FAIL", this);
+        EventManager.registerEvent("GAME_END", this);
+        
+
+        gui = new SettlersGUI();
+        gui.initialize();        
+    }
+    
 }
--- Settlers/game/gui/RollBox.java
+++ Settlers/game/gui/RollBox.java
@@ -1,18 +1,11 @@
 package settlers.game.gui;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.MediaTracker;
-import java.util.Random;
+import java.awt.Dimension;
 
-import javax.swing.ImageIcon;
+import javax.swing.WindowConstants;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
-import javax.swing.border.LineBorder;
-
-import settlers.game.GameState;
 
 /**
 * This code was edited or generated using CloudGarden's Jigloo
@@ -27,103 +20,42 @@ import settlers.game.GameState;
 * LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
 */
 public class RollBox extends javax.swing.JPanel {
-	
-	private JLabel rollBox;
-	private JTextArea rollHistory;
-    Image[] img = new Image[6]; // dice images are loaded into here. dice side 1 is img[0], side 2 is img[1], and so on
-    MediaTracker mt;
-    
-    GameBoard parent;
-    
-    Random rand;
-	int i;
-	private boolean rolled;
-	
-	int dice1, dice2;
-	private int rollValue;
-	
-	public RollBox(GameBoard _parent) {
-		super();
-		initGUI();
-		
-		parent = _parent;
-		
-		rand = new Random();
-		
-		rolled = false;
-		
-	    mt = new MediaTracker(this);
-	    for (i = 0; i <= 5; i++) {
-			img[i] = new ImageIcon(getClass().getResource("/settlers/game/images/d"+(i+1)+".png")).getImage();
-			mt.addImage(img[i],i);
-		}
-	}
-	
-	private void initGUI() {
-		try {
-			this.setPreferredSize(new java.awt.Dimension(120, 120));
-			this.setSize(120, 120);
-			
-			
-			
-			{
-				rollBox = new JLabel();
-				this.add(rollBox);
-				rollBox.setText("Roll Box");
-				rollBox.setPreferredSize(new java.awt.Dimension(93, 16));
-				rollBox.setHorizontalAlignment(SwingConstants.CENTER);
-				rollBox.setHorizontalTextPosition(SwingConstants.CENTER);
-			}
-		    
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		this.setVisible(false);
-	}
-	
-	public void update(Graphics g){
-			paint(g);
-	}
-	
-    public void paint(Graphics g) {
-		this.setBorder(new LineBorder(Color.black, 3));
-    	if(rolled)
-    	{
-			for (int x = 0; x < 10; x++) {
-				if (img[rand.nextInt(6)] != null) 
-				{ // makes sure there's something in each of the array positions
-					g.drawImage(img[rand.nextInt(6)],0,30,this); // where the first die is displayed in the frame
-					g.drawImage(img[rand.nextInt(6)],60,50,this); // where the second die is displayed in the frame
-					this.validate();
-					try {
-						Thread.currentThread();
-						Thread.sleep(75);
-						} // length of pauses between graphic cycles
-					catch (InterruptedException e) {e.printStackTrace();}
-				}  
-			}
-			g.drawImage(img[dice1 - 1],0,30,this); // displays the final dice 1
-			g.drawImage(img[dice2 - 1],60,50,this); // displays the final dice 2
-			this.validate();
-    	}
-    	rolled = false;
-	}
+    private JLabel rollBox;
 
-	public int rollDice() {
-		// TODO Auto-generated method stub
-		rolled = true;
-		
-		dice.roll(2);
-		
-		dice1 = dice.getD1();
-		dice2 = dice.getD2();
-		
-		rollValue = dice1 + dice2;
-		
-		System.out.println("Dice 1 is " + dice1 + ".");
-		System.out.println("Dice 2 is " + dice2 + ".\n");
-		repaint();
-		
-		return rollValue;
-	}	
+    /**
+    * Auto-generated main method to display this 
+    * JPanel inside a new JFrame.
+    */
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(new RollBox());
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    public RollBox() {
+        super();
+        initGUI();
+    }
+    
+    private void initGUI() {
+        try {
+            this.setPreferredSize(new java.awt.Dimension(100, 100));
+            this.setSize(100, 100);
+            this.setBackground(new java.awt.Color(192,192,192));
+            {
+                rollBox = new JLabel();
+                this.add(rollBox);
+                rollBox.setText("Roll Box");
+                rollBox.setPreferredSize(new java.awt.Dimension(93, 16));
+                rollBox.setHorizontalAlignment(SwingConstants.CENTER);
+                rollBox.setHorizontalTextPosition(SwingConstants.CENTER);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        this.setVisible(false);
+    }
+
 }
